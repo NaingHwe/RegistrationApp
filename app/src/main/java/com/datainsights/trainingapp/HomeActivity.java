2022a13390d1dd5866fdee3e45d58eba8f1f6925
NHO <nainghtwe002012@gmail.com>
2018-10-07 14:07:20 +0630

    Submitted New Code

--- app/src/main/java/com/datainsights/trainingapp/HomeActivity.java
+++ app/src/main/java/com/datainsights/trainingapp/HomeActivity.java
@@ -1,8 +1,8 @@
 package com.datainsights.trainingapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.datainsights.trainingapp.StudentRegistration.StudentListActivity;
--- app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentDetailActivity.java
+++ app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentDetailActivity.java
@@ -1,7 +1,7 @@
 package com.datainsights.trainingapp.StudentRegistration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.datainsights.trainingapp.R;
 
