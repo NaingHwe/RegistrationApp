14f5851448d3473e96a5194344d19607896db89c
THU THU MAUNG <ms.jellyfishgirl@gmail.com>
2018-10-07 16:37:49 +0630

    HideKeyboard and Added back button action

--- app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentDetailActivity.java
+++ app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentDetailActivity.java
@@ -2,14 +2,24 @@ package com.datainsights.trainingapp.StudentRegistration;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.datainsights.trainingapp.R;
 
 public class StudentDetailActivity extends AppCompatActivity {
 
+    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_detail);
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentListActivity.java
+++ app/src/main/java/com/datainsights/trainingapp/StudentRegistration/StudentListActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 import com.datainsights.trainingapp.R;
 import com.datainsights.trainingapp.RVStudentListAdapter;
@@ -15,8 +16,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class StudentListActivity extends AppCompatActivity {
-RecyclerView rvStudentList;
+    RecyclerView rvStudentList;
     FloatingActionButton fab;
+    Button btnRegisterBack;
 List<StudentList> studentListList = Arrays.asList(
         new StudentList("Mg Mg","R.drawable.profile_pic"),
         new StudentList("Mg Mg","R.drawable.profile_pic"),
@@ -31,10 +33,17 @@ List<StudentList> studentListList = Arrays.asList(
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
-
+        btnRegisterBack = findViewById(R.id.btn_RegisterBack);
      /*   Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
 
+        btnRegisterBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
          fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +54,7 @@ List<StudentList> studentListList = Arrays.asList(
                         .setAction("Action", null).show();*/
             }
         });
+
 
         rvStudentList = findViewById(R.id.rv_studentList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
